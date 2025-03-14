dc955895ad94cd97fd206a77c5e857fd68e72613
Badiane95 <badiane.falou95@gmail.com>
2025-03-14 14:20:05 +0100

    save

--- Assets/Scripts/Managers/CurrentSceneManager.cs
+++ Assets/Scripts/Managers/CurrentSceneManager.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class CurrentSceneManager : MonoBehaviour
 {
     public bool isDebugConsoleOpened = false;
+    public GameObject gameOverScreen;
+    public GameObject playerMovement;
+    public bool isPaused = false;
+    public GameObject pauseScreen; 
+    public VoidEventChannel onPlayerDeath;
+    public VoidEventChannel onPause;
+    public VoidEventChannel onResume;
 
     [Header("Listen to events")]
     public StringEventChannel onLevelEnded;
@@ -13,12 +20,22 @@ public class CurrentSceneManager : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         Time.timeScale = 1f;
+        gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
 
     private void OnEnable()
     {
         onLevelEnded.OnEventRaised += LoadScene;
         onDebugConsoleOpenEvent.OnEventRaised += OnDebugConsoleOpen;
+        onPlayerDeath.OnEventRaised += Die;
+    }
+
+    private void OnDisable()
+    {
+        onLevelEnded.OnEventRaised -= LoadScene;
+        onDebugConsoleOpenEvent.OnEventRaised -= OnDebugConsoleOpen;
+        onPlayerDeath.OnEventRaised -= Die;
     }
 
     public void LoadScene(string sceneName)
@@ -71,14 +88,45 @@ public class CurrentSceneManager : MonoBehaviour
 #endif
     }
 
-    private void OnDisable()
-    {
-        onLevelEnded.OnEventRaised -= LoadScene;
-        onDebugConsoleOpenEvent.OnEventRaised -= OnDebugConsoleOpen;
-    }
-
     private void OnDebugConsoleOpen(bool debugConsoleOpened)
     {
         isDebugConsoleOpened = debugConsoleOpened;
+    }
+
+    private void Die() 
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void Pause() 
+    {
+        if (Time.timeScale == 0) 
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            pauseScreen.SetActive(false);
+            onResume.Raise();
+        } 
+        else 
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            pauseScreen.SetActive(true);
+            onPause.Raise();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Pause();
+        }
+#if UNITY_EDITOR 
+        if(Input.GetKeyDown(KeyCode.R)) 
+        {
+            RestartLevel();
+        }
+#endif
     }
 }
