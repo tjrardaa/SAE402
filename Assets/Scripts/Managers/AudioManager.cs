c0352959fb974102e567c56a9c25f79eba743c94
Badiane95 <165165737+Badiane95@users.noreply.github.com>
2025-05-11 21:15:09 +0200

    save

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,67 +4,119 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource mainAudioSource;
-    public AudioMixerGroup soundEffectMixer;
-    public AudioMixerGroup musicEffectMixer;
-    public AudioClip[] playlist;
+    public static AudioManager Instance { get; private set; } // Singleton accessible partout
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioSource mainAudioSource;
+    [SerializeField] private AudioMixerGroup soundEffectMixer;
+    [SerializeField] private AudioMixerGroup musicEffectMixer;
+    [SerializeField] private AudioClip[] playlist;
+
+    [Header("Volume Control")]
+    [SerializeField][Range(0f, 1f)] private float volumeOnPaused = 0.35f;
+    [SerializeField][Range(0f, 1f)] private float volumeOnPlay = 1f;
+    [SerializeField] private float volumeStep = 0.005f;
+
+    [Header("Event Channels")]
+    [SerializeField] private PlaySoundAtEventChannel sfxAudioChannel;
+
     private int musicIndex;
-    private float volumeOnPaused = 0.35f;
-    private float volumeOnPlay = 1f;
-    private float volumeStep = 0.005f;
 
-    [Header("Listen to event channels")]
-    public PlaySoundAtEventChannel sfxAudioChannel;
+    //////////////////////////////////////////////////////////
+    #region Initialization & Singleton
+    //////////////////////////////////////////////////////////
 
-    private void OnEnable()
+    private void Awake()
     {
-        sfxAudioChannel.OnEventRaised += PlayClipAt;
+        ConfigureSingleton();
+        InitializeAudioSource();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void ConfigureSingleton()
     {
-        mainAudioSource.clip = playlist[0];
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Détruit les doublons
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // Garde l'instance entre les scènes
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeAudioSource()
     {
-        if (!mainAudioSource.isPlaying)
+        if (mainAudioSource == null)
+        {
+            mainAudioSource = GetComponent<AudioSource>();
+            if (mainAudioSource == null)
+            {
+                Debug.LogError("Aucun AudioSource trouvé sur le GameObject !");
+            }
+        }
+    }
+
+    #endregion
+
+    //////////////////////////////////////////////////////////
+    #region Editor Validation
+    //////////////////////////////////////////////////////////
+
+    private void OnValidate()
+    {
+        // Avertissements dans l'éditeur si champs non assignés
+        if (mainAudioSource == null) Debug.LogWarning("Assignez mainAudioSource !", this);
+        if (playlist.Length == 0) Debug.LogWarning("Playlist vide !", this);
+        if (sfxAudioChannel == null) Debug.LogWarning("Assignez sfxAudioChannel !", this);
+    }
+
+    #endregion
+
+    //////////////////////////////////////////////////////////
+    #region Music Management
+    //////////////////////////////////////////////////////////
+
+    private void Start()
+    {
+        if (playlist.Length > 0)
+        {
+            PlayMusic(0); // Démarre avec la première musique
+        }
+    }
+
+    private void Update()
+    {
+        if (!mainAudioSource.isPlaying && playlist.Length > 0)
         {
             PlayNextMusic();
         }
     }
 
-    IEnumerator IncreaseVolume()
+    private void PlayMusic(int index)
     {
-        while (mainAudioSource.volume < volumeOnPlay)
-        {
-            mainAudioSource.volume += volumeStep;
-            yield return null;
-        }
-    }
-
-    IEnumerator DecreaseVolume()
-    {
-        while (mainAudioSource.volume > volumeOnPaused)
-        {
-            mainAudioSource.volume -= volumeStep;
-            yield return null;
-        }
-    }
-
-    void PlayNextMusic()
-    {
-        musicIndex = (musicIndex + 1) % playlist.Length;
+        musicIndex = index;
         mainAudioSource.clip = playlist[musicIndex];
         mainAudioSource.outputAudioMixerGroup = musicEffectMixer;
         mainAudioSource.Play();
     }
 
+    private void PlayNextMusic()
+    {
+        musicIndex = (musicIndex + 1) % playlist.Length;
+        PlayMusic(musicIndex);
+    }
+
+    #endregion
+
+    //////////////////////////////////////////////////////////
+    #region Public Methods
+    //////////////////////////////////////////////////////////
+
     public void PlayClipAt(AudioClip clip, Vector3 position)
     {
-        GameObject tempGO = new GameObject("TempAudio");
+        if (clip == null) return;
+
+        GameObject tempGO = new GameObject("TempAudio: " + clip.name);
         tempGO.transform.position = position;
         AudioSource audioSource = tempGO.AddComponent<AudioSource>();
         audioSource.clip = clip;
@@ -75,21 +127,38 @@ public class AudioManager : MonoBehaviour
 
     public void OnTogglePause(bool isGamePaused)
     {
-        // AudioListener.pause = isGamePaused;
-        if (isGamePaused)
-        {
-            StopAllCoroutines();
-            StartCoroutine(DecreaseVolume());
-        }
-        else
-        {
-            StopAllCoroutines();
-            StartCoroutine(IncreaseVolume());
-        }
+        StopAllCoroutines();
+        StartCoroutine(AdjustVolume(isGamePaused ? volumeOnPaused : volumeOnPlay));
     }
 
-    private void OnDisable()
+    #endregion
+
+    //////////////////////////////////////////////////////////
+    #region Volume Fading
+    //////////////////////////////////////////////////////////
+
+    private IEnumerator AdjustVolume(float targetVolume)
     {
-        sfxAudioChannel.OnEventRaised -= PlayClipAt;
+        float currentVolume = mainAudioSource.volume;
+        float duration = Mathf.Abs(targetVolume - currentVolume) / volumeStep;
+
+        for (float t = 0; t < duration; t += Time.unscaledDeltaTime)
+        {
+            mainAudioSource.volume = Mathf.Lerp(currentVolume, targetVolume, t / duration);
+            yield return null;
+        }
+
+        mainAudioSource.volume = targetVolume;
     }
+
+    #endregion
+
+    //////////////////////////////////////////////////////////
+    #region Event Subscriptions
+    //////////////////////////////////////////////////////////
+
+    private void OnEnable() => sfxAudioChannel.OnEventRaised += PlayClipAt;
+    private void OnDisable() => sfxAudioChannel.OnEventRaised -= PlayClipAt;
+
+    #endregion
 }
